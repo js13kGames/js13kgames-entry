06427ec636edbc91a1950d225921d1629b2fdb7a
JuBzzz <jubz147@gmail.com>
2018-08-31 20:32:11 -0300

    Started to add the control for the canvas.

---
+++ main.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("screen");
+var ctx = canvas.getContext('2d');
+
+
+function start() {
+    ctx.fillStyle = "rgb(25,25,25)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  start();
