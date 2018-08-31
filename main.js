72a41417dc904ebe4a5a94a3c8aa52f990fa9516
JuBzzz <jubz147@gmail.com>
2018-08-31 20:40:02 -0300

    Added a Scene object.

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+var MENU = 0,
+    MISSION_SELECT = 1,
+    GAMEPLAY = 2;
+
+
+class Scene {
+    constructor(id) {
+        this.id = id;
+        this.ui = [];
+    }
+
+    load() {
+        ctx.fillStyle = "rgb(25,25,25)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+
 var canvas = document.getElementById("screen");
 var ctx = canvas.getContext('2d');
 
+var cur_scene = new Scene(MENU);
 
-function start() {
-    ctx.fillStyle = "rgb(25,25,25)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }
-
-  start();
+cur_scene.load();
