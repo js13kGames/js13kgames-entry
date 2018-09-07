b5c860aa5e26b8e3c272f6a0c4c50929dba6e7f3
JuBzzz <jubz147@gmail.com>
2018-09-07 14:07:43 -0300

    Added a better combat system.

--- entities.js
+++ entities.js
@@ -16,11 +16,16 @@ class Player extends Entity {
     }
     interactWith(other) {
         if (other.type == "enemy") {
-            other.hp -= DATA["version"];
+            var roll = randint(1, 21); // Attack roll
+            if (roll + DATA["version"] < other.ac) {
+                print_message(">> Missed your attack!")
+                return;
+            }
 
-            if (DATA["installed"].indexOf("Denial_Of_Service")) {
+            other.hp -= DATA["version"];
+            if (DATA["installed"].indexOf("Denial_Of_Service") && roll == 20) {
                 if (Math.random < 0.2) {
-                    print_message("Denial_Of_Service successfully shut down the enemy's connection");
+                    print_message(">> Denial_Of_Service successfully shut down the enemy's connection");
                     other.destroy = true;
                     DATA["bits"]++;
                 }
@@ -28,7 +33,7 @@ class Player extends Entity {
             if (other.hp == 0) {
                 other.destroy = true;
                 if (DATA["installed"].indexOf("Maintenance") && this.latency > 40) {
-                    print_message("Maintenance repairs your connection by 20ms.")
+                    print_message(">> Maintenance repairs your connection by 20ms.")
                     this.latency -= 20;
                 }
                 DATA["bits"]++;
@@ -38,7 +43,7 @@ class Player extends Entity {
             }
         } else if (other.type == "goal") {
             other.destroy = true;
-            print_message("You reached your goal!");
+            print_message(">> You reached your goal!");
             DATA["level"]++;
             startDungeon();
         }
@@ -46,17 +51,23 @@ class Player extends Entity {
 }
 
 class Enemy extends Entity {
-    constructor(dungeon, pos) {
+    constructor(dungeon, pos, hp=3, ac=12, att=1, dmg=20) {
         super(dungeon, pos)
-        this.hp = 3;
-        this.att = 20;
+        this.hp = hp;
+        this.ac = ac;  // Armor class
+        this.att = att;  // Accuracy
+        this.dmg = dmg; // Damage
         this.type = "enemy";
         this.status = "";
     }
     interactWith(other) {
         if (other.type == "player") {
+            if (randint(0, 20) < 10 + DATA["level"]) { // Attack roll, 10 is the base AC
+                print_message("<< Enemy tried to cut your connection, but failed!")
+                return;
+            }
             other.latency += this.att;
-            print_message("Enemy is trying to cut your connection, increased latency by 10ms.")
+            print_message("<< Enemy is trying to cut your connection, increased latency by 10ms!")
             playAnimation(0, 0, canvas.width,  [255,255,255], 0.2);
             if (other.latency == 0) { other.destroy = true; }
         }
